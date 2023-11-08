c3f0896e03bcee551ee6c1f0da2b43eb80605c7f
Alberto Barcenas <albertobcns@gmail.com>
2023-11-08 14:22:26 -0600

    -Ejercicio de ciclo de vida

---
+++ app/src/main/java/com/example/ejerciciociclosvida/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.ejerciciociclosvida
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bSecondActivity: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "onCreate")
+
+        bSecondActivity = findViewById(R.id.bSecondActivity)
+
+        bSecondActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+            //finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy")
+    }
+}
